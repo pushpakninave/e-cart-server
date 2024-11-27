0e0e91f2a376817c21f6990077ef7595b33e6898
pushpakninave <pushpakninave.jee@gmail.com>
2024-11-28 00:11:29 +0530

    this contains bean creation for ModelMapper and is an important and new concept for me atleast.

---
+++ src/main/java/com/pushpak/e_cart/config/ShopConfig.java
@@ -0,0 +1,15 @@
+package com.pushpak.e_cart.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopConfig {
+
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
